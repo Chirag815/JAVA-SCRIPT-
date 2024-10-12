bbce4a831cc9627aa3eb4e162fbfe3ce0323ca39
Chirag815 <144776714+Chirag815@users.noreply.github.com>
2024-10-12 16:07:47 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 837829
+let accountEmail = "chiraggarg@gmail.com"
+var accountPassword = "82367289"
+accountCity = "jaipur"
+
+// accountId=2
+accountEmail="garg@gmail.com"
+accountPassword="789"
+accountCity="Dadri"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity
+
+])
---
+++ 01_basics/02_datatTypes.js
@@ -0,0 +1,5 @@
+"use strict"; //treat all code of js as newer version
+
+//alert(3+3)
+
+console.log();
